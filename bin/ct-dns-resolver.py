a6a0448a76db599656df617db2c34548b7f3cdfc
Alexandre Dulaunoy <a@foo.be>
2021-06-29 08:11:23 +0200

    new: [ct-dns-resolver] use standard Passive DNS common output format for the result

--- bin/ct-dns-resolver.py
+++ bin/ct-dns-resolver.py
@@ -10,7 +10,8 @@ import base64
 import os
 import dns.resolver
 import xxhash
-
+import time
+import json
 
 #r = redis.Redis(host='localhost', port=6379, db=15, socket_keepalive=True, socket_timeout=300)
 r = redis.Redis(unix_socket_path="/tmp/redis.sock")
@@ -20,11 +21,12 @@ p.subscribe('ct-certs', ignore_subscribe_messages=False)
 
 m = False
 
-common_names = ['www', 'mail', '', 'host', 'router', 'ns', 'gw', 'server']
+common_names = ['www', 'mail', '', 'host', 'router', 'ns', 'gw', 'server', 'gateway']
 
 resolver = dns.resolver.Resolver()
-resolver.timeout = 1
-resolver.lifetime = 1
+resolver.timeout = 0.2
+resolver.lifetime = 0.2
+resolver.nameservers = ['185.194.93.128']
 time_to_expire = 3000
 
 def cache(value = None, time_to_expire = time_to_expire):
@@ -45,7 +47,7 @@ while True:
 
     if m:
         if type(m['data']) is int:
-            print ("Skip non-related pub-sub message")
+            #print("Skip non-related pub-sub message")
             continue
         cert_der = base64.b64decode(m['data'].rstrip())
         #decode(cert_der = cert_der)
@@ -53,7 +55,7 @@ while True:
         try:
             subject = x509.get_subject().as_text()
         except:
-            print("!!! Cannot get subject")
+            pass
 
         try:
             cAltName = x509.get_ext('subjectAltName').get_value()
@@ -65,7 +67,6 @@ while True:
                 dns_to_query = dns_entry.replace("*", common_name)
                 hash_dns_to_query = xxhash.xxh128_hexdigest(dns_to_query)
                 if r.exists(hash_dns_to_query):
-                    print("Already seen")
                     continue
                 cache(value=hash_dns_to_query)
                 if dns_to_query.startswith('.'):
@@ -79,14 +80,25 @@ while True:
                     #print("-- NX {}".format(dns_to_query))
                 if answers is None: continue
                 for rdata in answers:
-                    print(rdata)
+                    pdns_cof_record = {}
+                    ts = time.time()
+                    pdns_cof_record['time_first'] = ts
+                    pdns_cof_record['time_last'] = ts
+                    pdns_cof_record['rrtype'] = 'AAAA'
+                    pdns_cof_record['rrname'] = dns_to_query
+                    pdns_cof_record['rdata'] = str(rdata)
+                    pdns_cof_record['sensor_id'] = 'circl-feed-ipv6-ct-newlyseen-aaaa'
+                    r.publish('circl-feed-ipv6-ct-newlyseen-aaaa', json.dumps(pdns_cof_record))
+                    print(json.dumps(pdns_cof_record))
 
         for altname in cAltName.split(','):
-            to_query = altname.split(":")[1]
+            try:
+                to_query = altname.split(":")[1]
+            except:
+                continue
             r.ping()
             hash_to_query = xxhash.xxh128_hexdigest(to_query)
             if r.exists(hash_to_query):
-                print("Already seen")
                 continue
             cache(value=hash_to_query)
             answers = None
@@ -94,10 +106,19 @@ while True:
                 answers = resolver.resolve(to_query, 'AAAA')
             except:
                 pass
-                #print("-- NX {}".format(to_query))
             if answers is None: continue
             for rdata in answers:
-                print(rdata)
+                pdns_cof_record = {}
+                ts = time.time()
+                pdns_cof_record['time_first'] = ts
+                pdns_cof_record['time_last'] = ts
+                pdns_cof_record['rrtype'] = 'AAAA'
+                pdns_cof_record['rrname'] = to_query
+                pdns_cof_record['rdata'] = str(rdata)
+                pdns_cof_record['sensor_id'] = 'circl-feed-ipv6-ct-newlyseen-aaaa'
+                r.publish('circl-feed-ipv6-ct-newlyseen-aaaa', json.dumps(pdns_cof_record))
+                print(json.dumps(pdns_cof_record))
 
-    time.sleep(0.001)
+
+    time.sleep(0.0001)
 
